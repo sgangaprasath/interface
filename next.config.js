fac75b18d5d8b3a6da45b9af42b7746d2ee31f1c
S Ganga Prasath <s.gangaprasath1@gmail.com>
2024-01-30 10:01:29 +0530

    smallChange

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-  webpack: (config) => {
-    let modularizeImports = null;
-    config.module.rules.some((rule) =>
-      rule.oneOf?.some((oneOf) => {
-        modularizeImports =
-          oneOf?.use?.options?.nextConfig?.modularizeImports;
-        return modularizeImports;
-      }),
-    );
-    if (modularizeImports?.["@headlessui/react"])
-      delete modularizeImports["@headlessui/react"];
-    return config;
-  },
+  // webpack: (config) => {
+  //   let modularizeImports = null;
+  //   config.module.rules.some((rule) =>
+  //     rule.oneOf?.some((oneOf) => {
+  //       modularizeImports =
+  //         oneOf?.use?.options?.nextConfig?.modularizeImports;
+  //       return modularizeImports;
+  //     }),
+  //   );
+  //   if (modularizeImports?.["@headlessui/react"])
+  //     delete modularizeImports["@headlessui/react"];
+  //   return config;
+  // },
 }
 module.exports = nextConfig
