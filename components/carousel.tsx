eed20293762ab7b9de6f7e4d671affbd2077497a
S Ganga Prasath <s.gangaprasath1@gmail.com>
2024-03-03 08:14:30 +0530

    PhotoAddition

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import Img01 from "@/public/gallery/gingee.jpg";
 import Img02 from "@/public/gallery/cricut.jpg";
-
+import Img03 from "@/public/gallery/toscanoDinner.jpg";
 export default function Csel() {
   return (
     <Carousel>
@@ -33,6 +33,20 @@ export default function Csel() {
         <div className="absolute top-0 px-4 py-2 sm:opacity-0 md:opacity-100 md:bg-gray-800/40 w-full">
           <h1 className="text-white text-xs">
             Paul Jackson insipired Kirigami art by Jay Chavda
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap items-center justify-center h-full bg-gray-400">
+        <Image
+          src={Img02}
+          width={0}
+          height={0}
+          alt={"Our dinner at Toscano with AM1100 TAs"}
+        />
+        <div className="absolute top-0 px-4 py-2 sm:opacity-0 md:opacity-100 md:bg-gray-800/40 w-full">
+          <h1 className="text-white text-xs">
+            Our dinner at Toscano with AM1100 TAs
           </h1>
         </div>
       </div>
