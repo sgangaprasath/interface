49bcc16e588c2a9714c4eb08082bedaf2999e899
S Ganga Prasath <s.gangaprasath1@gmail.com>
2026-07-27 12:41:11 +0530

    CleanUp

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const isProd = process.env.GITHUB_ACTIONS === 'true'
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: isProd ? '/interface' : '',
-  assetPrefix: isProd ? '/interface' : '',
+  basePath: '',
 }
 
 module.exports = nextConfig
