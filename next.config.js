4710fe0c029e963598062084f58ffaf72bb60c3c
S Ganga Prasath <s.gangaprasath1@gmail.com>
2026-07-27 10:59:26 +0530

    fix basepath and deploy to subfolder

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const isProd = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: '',
+  basePath: isProd ? '/interface' : '',
+  assetPrefix: isProd ? '/interface' : '',
 }
 
 module.exports = nextConfig
